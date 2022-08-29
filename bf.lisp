5181ff84eb5caa763a4c2641396e13163dbb5673
alve1801 <alex1velkov@gmail.com>
2022-08-29 22:35:06 +0200

    Update bf.lisp

--- bf.lisp
+++ bf.lisp
@@ -1,57 +1,62 @@
+(defun inc lambda (val) ((val)))
+
+(defun dec lambda (val) (car val))
+
+(defun moar lambda (a b) (cond
+	((eq a nil) nil)
+	((eq b nil) t)
+	(t (moar (dec a) (dec b)))
+))
+
 (defun get_at lambda (index list)
 	(cond
-		((eq index 0) (car list))
-		(t (get_at (- index 1) (cdr list)))
+		((eq index nil) (car list))
+		(t (get_at (dec index) (cdr list)))
 	)
 )
 
 (defun set_at lambda (val index list)
 	(cond
-		((eq index 0) (cons val (cdr list)))
-		(t (cons (car list) (at val (- index 1) (cdr list))))
+		((eq index nil) (cons val (cdr list)))
+		(t (cons (car list) (at val (dec index) (cdr list))))
 	)
 )
 
-(defun len lambda (list) (cond (list (+ 1 (len (cdr list))))(t 0)))
+(defun len lambda (list) (cond (list (inc (len (cdr list))))(t nil)))
 
-(defun append lambda (list) (cond (list (cons (car list) (append (cdr list)))) (t 0)))
+(defun append lambda (list) (cond (list (cons (car list) (append (cdr list)))) (t (nil))))
 
 (defun fwd lambda (prog pp val) (cond (val (cond
-	((eq (get_at pp prog) (quote [)) (fwd prog (+ pp 1) (+ val 1)))
-	((eq (get_at pp prog) (quote ])) (fwd prog (+ pp 1) (- val 1)))
-	(t (fwd prog (+ pp 1) val))
+	((eq (get_at pp prog) (quote [)) (fwd prog (inc pp) (inc val)))
+	((eq (get_at pp prog) (quote ])) (fwd prog (inc pp) (dec val)))
+	(t (fwd prog (inc pp) val))
 )) (t pp)))
 
 (defun bck lambda (prog pp val) (cond (val (cond
-	((eq (get_at pp prog) (quote [)) (fwd prog (+ pp 1) (- val 1)))
-	((eq (get_at pp prog) (quote ])) (fwd prog (+ pp 1) (+ val 1)))
-	(t (fwd prog (- pp 1) val))
+	((eq (get_at pp prog) (quote [)) (fwd prog (inc pp) (dec val)))
+	((eq (get_at pp prog) (quote ])) (fwd prog (inc pp) (inc val)))
+	(t (fwd prog (dec pp) val))
 )) (t pp)))
 
 (defun bf_eval lambda (prog pp stack sp)
 	(cond
-		((> pp (len prog)) stack)
-		((eq (get_at pp prog) (quote +)) (bf_eval prog (+ pp 1) (set_at (+ (get_at sp stack) 1) sp stack) sp))
-		((eq (get_at pp prog) (quote -)) (bf_eval prog (+ pp 1) (set_at (- (get_at sp stack) 1) sp stack) sp))
-		((eq (get_at pp prog) (quote <)) (bf_eval prog (+ pp 1) stack (- sp 1)))
-		((eq (get_at pp prog) (quote >)) (bf_eval prog (+ pp 1) (cond ((> sp (len stack)) (append stack)) (t stack)) (+ sp 1)))
+		((moar pp (len prog)) stack)
+		((eq (get_at pp prog) (quote +)) (bf_eval prog (inc pp) (set_at (inc (get_at sp stack)) sp stack) sp))
+		((eq (get_at pp prog) (quote -)) (bf_eval prog (inc pp) (set_at (dec (get_at sp stack)) sp stack) sp))
+		((eq (get_at pp prog) (quote <)) (bf_eval prog (inc pp) stack (dec sp)))
+		((eq (get_at pp prog) (quote >)) (bf_eval prog (inc pp) (cond ((moar sp (len stack)) (append stack)) (t stack)) (inc sp)))
 		((eq (get_at pp prog) (quote [)) (cond
-			((get_at sp stack) (bf_eval prog (+ pp 1) stack sp))
-			(t ((bf_eval prog (fwd prog pp 1) stack sp)))
+			((get_at sp stack) (bf_eval prog (inc pp) stack sp))
+			(t ((bf_eval prog (fwd prog pp (nil)) stack sp)))
 		))
 		((eq (get_at pp prog) (quote ])) (cond
-			((get_at sp stack) ((bf_eval prog (bck prog pp 1) stack sp)))
-			(t (bf_eval prog (+ pp 1) stack sp))
+			((get_at sp stack) ((bf_eval prog (bck prog pp (nil)) stack sp)))
+			(t (bf_eval prog (inc pp) stack sp))
 		))
-		((eq (get_at pp prog)) (quote X) (bf_eval prog (+ pp 1) stack sp))
+		(t stack)
 	)
 )
 
-(defun brainfuck lambda (prog) (bf_eval prog 0 (0) 0))
+(defun brainfuck lambda (prog) (bf_eval prog nil (nil) nil))
 
 (brainfuck (quote (+++++++[>+++++++<-]>.)))
-
-
-(comment assuming prog is a lisp of valid brainfuck instructions, (brainfuck prog) returns the stack after prog is done executing)
-(comment valid meaning that its brackets are balanced and it doesnt go into negative tape space)
-(comment also ive been referring to it as a stack when its actually a tape, please ignore that)
